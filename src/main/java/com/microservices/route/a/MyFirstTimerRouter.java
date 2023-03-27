6c0fadb5e245c4ec9aa9c5f0864c10a584e063e7
karangurnani <karang393@gmail.com>
2023-03-27 15:53:43 +0530

    MyFirstTimerRouter

--- src/main/java/com/microservices/route/a/MyFirstTimerRouter.java
+++ src/main/java/com/microservices/route/a/MyFirstTimerRouter.java
@@ -1,18 +1,24 @@
 package com.microservices.route.a;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
 
 import java.time.LocalDateTime;
-import java.util.logging.Logger;
 
 @Component
 public class MyFirstTimerRouter extends RouteBuilder {
 
     @Autowired
     private GetCurrentTimeBean getCurrentTimeBean;
+
+    @Autowired
+    private SimpleLoggingProcessingComponent loggingComponent;
+
     @Override
     public void configure() throws Exception {
         //timer
@@ -29,8 +35,11 @@ public class MyFirstTimerRouter extends RouteBuilder {
                 //Types of operation
                 //Processing - When body doesn't change
                 //Transformation - When body changes
-                .bean(getCurrentTimeBean,"getCurrentTime")
+                .bean(getCurrentTimeBean, "getCurrentTime")
                 .log("${body}")
+                .bean(loggingComponent)
+                .log("${body}")
+                .process(new SimpleLoggingProcessor())
                 .to("log:first-timer");
 
     }
@@ -41,13 +50,25 @@ class GetCurrentTimeBean {
     public String getCurrentTime() {
         return "Time now is " + LocalDateTime.now();
     }
+
+
 }
 
 @Component
 class SimpleLoggingProcessingComponent {
 
     private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+
     public void process(String message) {
         logger.info("SimpleLoggingProcessingComponent {}", message);
+    }
+}
+
+class SimpleLoggingProcessor implements Processor {
+
+    private Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        logger.info("SimpleLoggingProcessor {}", exchange);
     }
 }
